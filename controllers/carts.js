b67e5b84424c912e4d2bb7a4020f09882751bf9e
cdirks4 <cdirks4@me.com>
2021-07-08 16:47:33 -0400

    add and remove item from cart.

--- controllers/carts.js
+++ controllers/carts.js
@@ -12,7 +12,7 @@ router.get('/:id', (req, res, next) => {
 router.post('/', async (req, res, next) => {
 	try {
 		const found = await Cart.findOne({ owner: req.body.owner });
-		console.log(found);
+
 		if (!found) {
 			const newCart = await Cart.create(req.body);
 			return res.json(newCart);
@@ -24,11 +24,41 @@ router.post('/', async (req, res, next) => {
 		}
 	} catch {}
 });
+router.patch('/:id', async (req, res, next) => {
+	try {
+		let cart = await Cart.findOne({
+			_id: req.params.id,
+		})
+			.populate('products')
+			.exec();
+		if (!cart) throw new Error('Product not found in the cart found');
+		cart.products.splice(req.body.index, 1);
+		cart.save();
+		res.json(cart);
+	} catch (error) {
+		res.json(error);
+	}
+});
+router.put('/:id', async (req, res, next) => {
+	try {
+		let cart = await Cart.findOne({ _id: req.params.id });
+		if (!cart) throw new Error('Cart not found');
+		cart.products.push(req.body.prod_id);
+		console.log(cart);
+		cart.save();
+		let newCart = await Cart.findOne({ _id: req.params.id })
+			.populate('products')
+			.exec();
+		res.json(newCart);
+	} catch (error) {
+		res.json(error);
+	}
+});
 
 router.delete('/:id', (req, res, next) => {
 	Cart.findOneAndDelete({ owner: req.params.id })
-		.then((res) => (res.paid = true))
-		.then((res) => res.json(res));
+		.then((cart) => (cart.paid = true))
+		.then((cart) => res.json(cart));
 });
 
 module.exports = router;
